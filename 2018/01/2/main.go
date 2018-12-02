b3631c1393832b928f58d41b8e34aca506eb9654
xDimGG <skyuphigh01@gmail.com>
2018-12-01 22:01:21 -0500

    cleanup day 1 part 2's code

--- 2018/01/2/main.go
+++ 2018/01/2/main.go
@@ -13,29 +13,32 @@ func main() {
 		panic(err)
 	}
 
-	values := bytes.Split(input, []byte{'\n'})
+	i, err := firstDuplicate(bytes.Split(input, []byte{'\n'}))
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Println("Duplicate:", firstDuplicate(values))
+	fmt.Println("Duplicate:", i)
 }
 
-func firstDuplicate(values [][]byte) int {
+func firstDuplicate(values [][]byte) (i int, err error) {
 	seen := make(map[int]struct{})
 	acc := 0
 
 	for {
 		for _, v := range values {
-			i, err := strconv.Atoi(string(v))
+			i, err = strconv.Atoi(string(v))
 			if err != nil {
-				panic(err)
+				return
 			}
 
 			acc += i
 
 			if _, ok := seen[acc]; ok {
-				return acc
-			} else {
-				seen[acc] = struct{}{}
+				return acc, nil
 			}
+
+			seen[acc] = struct{}{}
 		}
 	}
 }
