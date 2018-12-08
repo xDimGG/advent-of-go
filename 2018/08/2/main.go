89cf56580de0c28e2252d3035eda3e347e3ab954
xDimGG <skyuphigh01@gmail.com>
2018-12-08 10:44:10 -0500

    day 8

---
+++ 2018/08/2/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+)
+
+type node struct {
+	children []*node
+	metadata []byte
+}
+
+func main() {
+	input, err := ioutil.ReadFile("../input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	data := make([]byte, 0)
+
+	for _, raw := range bytes.Split(input, []byte{' '}) {
+		num, err := strconv.Atoi(string(raw))
+		if err != nil {
+			panic(err)
+		}
+
+		data = append(data, byte(num))
+	}
+
+	n, _ := read(data)
+	fmt.Println("Result:", sum(n))
+}
+
+func read(data []byte) (n *node, size int) {
+	children := int(data[0])
+	metadata := int(data[1])
+	n = &node{
+		children: make([]*node, children),
+		metadata: make([]byte, metadata),
+	}
+
+	data = data[2:]
+	size += 2
+
+	for i := 0; i < children; i++ {
+		child, childSize := read(data)
+		n.children[i] = child
+
+		data = data[childSize:]
+		size += childSize
+	}
+
+	n.metadata = data[:metadata]
+	size += metadata
+
+	return
+}
+
+func sum(n *node) (total int) {
+	if len(n.children) == 0 {
+		for _, d := range n.metadata {
+			total += int(d)
+		}
+	} else {
+		for _, d := range n.metadata {
+			if len(n.children) < int(d) {
+				continue
+			}
+
+			total += sum(n.children[d-1])
+		}
+	}
+
+	return total
+}
